75c71aec0c480a832d13598cc5d6088f26ad489e
Sindiso <mkhsin035@myuct.ac.za>
2025-08-19 16:40:08 +0200

    Added guard_id and target_id to parameters file. Added entry point to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+import os
+from glob import glob
 package_name = 'ringattractor'
 
 setup(
@@ -10,16 +11,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name), ['parameters.json']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sindiso',
-    maintainer_email='mkhsin035@myuct.ac.za',
+    maintainer_email='sindiso.mkhatshwa@uni-konstanz.de',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ringattractor = ringattractor.ringattractor:main',
         ],
     },
 )
